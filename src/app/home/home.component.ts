328589d0df2129056b2020feb8454283246c74bd
sharaharan <48280933+sharaharan@users.noreply.github.com>
2020-07-12 10:50:33 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,31 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
+import { HttpClient } from '@angular/common/http';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
     user: User;
+    selectedFile: File = null;
+    constructor(private http: HttpClient,accountService: AccountService){
+        
+     }
 
-    constructor(private accountService: AccountService) {
-        this.user = this.accountService.userValue;
+    onFileSelected(event){
+       this.selectedFile =<File>event.target.files[0];
     }
+    onUpload(){
+        const fd = new FormData();
+        fd.append('image',this.selectedFile,this.selectedFile.name);
+        this.http.post('',fd).subscribe(res=>{
+            console.log(res);
+        });
+
+    }
+
+    //  constructor(private accountService: AccountService) {
+    //      this.user = this.accountService.userValue;
+    //  }
 }
